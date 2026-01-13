d1a05455dd58d995cabecee9cea970b4cce0ac20
Tomppahh <Tommi.Paakkunainen@student.lut.fi>
2026-01-13 22:45:58 +0200

    bugfix for Task

--- W8/src/routes/user.ts
+++ W8/src/routes/user.ts
@@ -48,43 +48,37 @@ router.post(
 	}
 );
 
-router.post(
-	'/login',
-	registerValidator('email').isEmail(),
-	registerValidator('username').isLength({ min: 3, max: 25 }),
-	passwordValidator('password'),
-	async (req: Request, res: Response) => {
-		const errors: Result<ValidationError> = validationResult(req);
+router.post('/login', registerValidator('email').isEmail(), passwordValidator('password'), async (req: Request, res: Response) => {
+	const errors: Result<ValidationError> = validationResult(req);
 
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
-
-		try {
-			const user = await User.findOne({ email: req.body.email });
-			if (!user) {
-				return res.status(404).json({ message: 'User not found' });
-			}
-
-			const match = bcrypt.compareSync(req.body.password, user.password);
-			if (!match) {
-				return res.status(401).json({ message: 'Invalid credentials' });
-			}
-
-			const payload = {
-				_id: user._id,
-				username: user.username,
-				isAdmin: user.isAdmin,
-			};
-			console.log(payload);
-			const token = jwt.sign(payload, process.env.SECRET as string);
-			return res.status(200).json({ token: token });
-		} catch (error: any) {
-			console.error(`Error during login: ${error}`);
-			return res.status(500).json({ error: 'Internal Server Error' });
-		}
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
 	}
-);
+
+	try {
+		const user = await User.findOne({ email: req.body.email });
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+
+		const match = bcrypt.compareSync(req.body.password, user.password);
+		if (!match) {
+			return res.status(401).json({ message: 'Invalid credentials' });
+		}
+
+		const payload = {
+			_id: user._id,
+			username: user.username,
+			isAdmin: user.isAdmin,
+		};
+		console.log(payload);
+		const token = jwt.sign(payload, process.env.SECRET as string);
+		return res.status(200).json({ token: token });
+	} catch (error: any) {
+		console.error(`Error during login: ${error}`);
+		return res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
 router.get('/list', async (req: Request, res: Response) => {
 	try {
 		const users = await User.find({}, { password: 0 });
