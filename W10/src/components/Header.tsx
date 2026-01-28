9586b53edfd5049ba4d2db05c189866a646503ff
Tomppahh <Tommi.Paakkunainen@student.lut.fi>
2026-01-28 20:32:59 +0200

    bugfix

--- W10/src/components/Header.tsx
+++ W10/src/components/Header.tsx
@@ -5,31 +5,29 @@ function Header() {
 	const { t, i18n } = useTranslation();
 
 	return (
-		<div className="header-bg">
-			<header className="header">
-				<h1>Week 10</h1>
-				<nav>
-					<ul>
-						<li>
-							<a href="/">{t('header1')}</a>
-						</li>
-						<li>
-							<a href="/about">{t('header2')}</a>
-						</li>
-						<li>
-							<button id="fi" onClick={() => i18n.changeLanguage('fi')}>
-								FI
-							</button>
-						</li>
-						<li>
-							<button id="en" onClick={() => i18n.changeLanguage('en')}>
-								EN
-							</button>
-						</li>
-					</ul>
-				</nav>
-			</header>
-		</div>
+		<header className="header">
+			<h1>Week 10</h1>
+			<nav>
+				<ul>
+					<li>
+						<a href="/">{t('header1')}</a>
+					</li>
+					<li>
+						<a href="/about">{t('header2')}</a>
+					</li>
+					<li>
+						<button id="fi" onClick={() => i18n.changeLanguage('fi')}>
+							FI
+						</button>
+					</li>
+					<li>
+						<button id="en" onClick={() => i18n.changeLanguage('en')}>
+							EN
+						</button>
+					</li>
+				</ul>
+			</nav>
+		</header>
 	);
 }
 
