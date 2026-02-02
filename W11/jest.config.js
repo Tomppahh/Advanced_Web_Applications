c7dd1cf2374d1dbf21119b0d9a1b399e67a5530d
Tomppahh <Tommi.Paakkunainen@student.lut.fi>
2026-02-02 22:23:58 +0200

    debugging Task 1

--- W11/jest.config.js
+++ W11/jest.config.js
@@ -1,20 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+export default {
 	preset: 'ts-jest',
 	testEnvironment: 'jsdom',
 	moduleNameMapper: {
 		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
 	},
+	reporters: ['default'],
 	transform: {
 		'^.+\\.tsx?$': [
 			'ts-jest',
 			{
-				tsconfig: {
-					jsx: 'react-jsx',
-					esModuleInterop: true,
-					verbatimModuleSyntax: false,
-				},
+				tsconfig: '<rootDir>/tsconfig.app.json',
 			},
 		],
 	},
+	roots: ['<rootDir>/src/__test__'],
+	testTimeout: 10000,
 };
